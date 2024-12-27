ecc8c9baa532ef362b75d7eae57b966e79c8652a
signos-vitales-app <Signosvitales.la2@gmail.com>
2024-12-27 18:46:24 -0500

    searchuser

--- src/pages/SearchUsers.jsx
+++ src/pages/SearchUsers.jsx
@@ -22,7 +22,15 @@ const SearchUsers = () => {
         const fetchUsers = async () => {
             try {
                 const response = await getUsers();
-                setUsers(response.data);
+
+                // Ordenar los usuarios por username (nombre)
+                const sortedUsers = response.data.sort((a, b) => {
+                    if (a.username.toLowerCase() < b.username.toLowerCase()) return -1;
+                    if (a.username.toLowerCase() > b.username.toLowerCase()) return 1;
+                    return 0;
+                });
+
+                setUsers(sortedUsers); // Guardar los usuarios ordenados
             } catch (error) {
                 setError("Error al obtener los usuarios");
             }
@@ -90,39 +98,41 @@ const SearchUsers = () => {
     };
 
     return (
-        <div className="flex flex-col items-center justify-start min-h-screen bg-white-50 p-6">
+        <div className="flex flex-col items-center justify-start min-h-screen bg-gray-50 p-6">
             <ToastContainer />
-            <h1 className="text-4xl font-bold mb-3 mt-10 text-blue-800">Usuarios Registrados</h1>
+            <h1 className="text-4xl font-bold mb-6 mt-10 text-blue-700">Usuarios Registrados</h1>
             {error && <p className="text-red-500">{error}</p>}
 
-            <div className="w-full max-w-7xl overflow-auto mt-4" style={{ scrollbarGutter: "stable" }}>
-                <div className="bg-white shadow-2xl rounded-lg border border-gray-200 max-h-[calc(100vh-200px)]">
-                    <table className="min-w-full table-fixed border-collapse text-black-700">
-                        <thead className="bg-gradient-to-r from-blue-700 to-blue-500 text-white sticky top-0 z-10 shadow-md">
+            <div className="w-full max-w-7xl overflow-auto mt-4">
+                <div className="bg-white shadow-2xl rounded-lg border border-gray-300">
+                    <table className="min-w-full border-collapse rounded-lg overflow-hidden">
+                        {/* Cabecera */}
+                        <thead className="bg-gradient-to-r from-blue-500 to-blue-600 text-white">
                             <tr>
-                                <th className="p-4 text-center text-sm font-semibold w-1/6">Nombre</th>
-                                <th className="p-4 text-center text-sm font-semibold w-1/6">Cédula</th>
-                                <th className="p-4 text-center text-sm font-semibold w-1/4">Correo</th>
-                                <th className="p-4 text-center text-sm font-semibold w-1/6">Rol</th>
-                                <th className="p-4 text-center text-sm font-semibold w-1/8">Estado</th>
-                                <th className="p-4 text-center text-sm font-semibold w-1/12">Editar</th>
-                                <th className="p-4 text-center text-sm font-semibold w-1/6">Acciones</th>
+                                <th className="p-4 text-center text-sm font-semibold tracking-wide ">Nombre</th>
+                                <th className="p-4 text-center text-sm font-semibold tracking-wide ">Cédula</th>
+                                <th className="p-4 text-center text-sm font-semibold tracking-wide ">Correo</th>
+                                <th className="p-4 text-center text-sm font-semibold tracking-wide ">Rol</th>
+                                <th className="p-4 text-center text-sm font-semibold tracking-wide ">Estado</th>
+                                <th className="p-4 text-center text-sm font-semibold tracking-wide ">Editar</th>
+                                <th className="p-4 text-center text-sm font-semibold tracking-wide ">Acciones</th>
                             </tr>
                         </thead>
-                        <tbody className="bg-gray-50 divide-y divide-gray-200">
+
+                        {/* Cuerpo */}
+                        <tbody>
                             {users.map((user, index) => (
                                 <tr
                                     key={user.id}
-                                    className={`h-16 border-b ${index % 2 === 0 ? "bg-gray-100" : "bg-white"
-                                        } hover:bg-gray-200 transition-colors`}
+                                    className={`h-16 border-b ${index % 2 === 0 ? "bg-gray-50" : "bg-white"} hover:bg-blue-50 transition-colors duration-200`}
                                 >
-                                    <td className="p-4 text-center text-sm font-medium">{user.username}</td>
-                                    <td className="p-4 text-center text-sm">{user.numero_identificacion}</td>
-                                    <td className="p-4 text-center text-sm">{user.email}</td>
-                                    <td className="p-4 text-center text-sm">{roleNames[user.role]}</td>
-                                    <td className="p-4 text-center text-sm">
+                                    <td className="p-4 text-center text-sm font-medium text-gray-700">{user.username}</td>
+                                    <td className="p-4 text-center text-sm text-gray-600">{user.numero_identificacion}</td>
+                                    <td className="p-4 text-center text-sm text-gray-600">{user.email}</td>
+                                    <td className="p-4 text-center text-sm font-semibold text-gray-800">{roleNames[user.role]}</td>
+                                    <td className="p-4 text-center">
                                         <span
-                                            className={`inline-block px-4 py-2 rounded-full text-sm font-semibold ${user.is_active ? "bg-green-500 text-white shadow-lg" : "bg-red-500 text-white shadow-lg"
+                                            className={`inline-block px-4 py-2 rounded-full text-sm font-semibold ${user.is_active ? "bg-green-500 text-white" : "bg-red-500 text-white"
                                                 }`}
                                         >
                                             {user.is_active ? "Activo" : "Inactivo"}
@@ -131,33 +141,39 @@ const SearchUsers = () => {
                                     <td className="p-4 text-center">
                                         <span
                                             onClick={() => navigate(`/edit-user/${user.id}`)}
-                                            className="inline-flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full text-blue-600 hover:text-blue-800 hover:bg-blue-200 hover:scale-110 transition-transform cursor-pointer"
+                                            className="inline-flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full text-blue-600 hover:text-blue-800 hover:bg-blue-200 transition-transform transform hover:scale-105 cursor-pointer"
                                             title="Editar usuario"
                                         >
                                             <MdOutlineEdit size={20} />
                                         </span>
                                     </td>
-                                    <td className="p-4 flex justify-center gap-4">
+
+                                    <td className="p-4 flex justify-center gap-3">
+                                        {/* Botón Activar/Desactivar */}
                                         <button
                                             onClick={() => handleToggleStatus(user.id, user.is_active)}
-                                            className={`flex items-center px-4 py-3 text-sm font-bold rounded-full shadow-lg transition ${user.is_active ? "bg-gray-600 text-white hover:bg-gray-700" : "bg-green-600 text-white hover:bg-green-700"
+                                            className={`flex items-center px-4 py-2 text-sm font-medium rounded-md shadow-lg transition-all duration-300 ease-in-out ${user.is_active
+                                                    ? "bg-gray-600 text-white hover:bg-gray-900 hover:scale-105"
+                                                    : "bg-green-600 text-white hover:bg-green-700 hover:scale-105"
                                                 }`}
                                         >
                                             {user.is_active ? (
                                                 <>
-                                                    <FiUserX className="mr-2" size={18} /> Desactivar
+                                                    <FiUserX className="mr-2 text-white" size={16} /> Desactivar
                                                 </>
                                             ) : (
                                                 <>
-                                                    <FiUserCheck className="mr-1" size={18} /> Activar
+                                                    <FiUserCheck className="mr-2 text-white" size={16} /> Activar
                                                 </>
                                             )}
                                         </button>
+
+                                        {/* Botón Eliminar */}
                                         <button
                                             onClick={() => handleDeleteUser(user.id)}
-                                            className="flex items-center px-4 py-3 bg-red-600 text-white rounded-full text-sm font-bold shadow-lg hover:bg-red-700 transition"
+                                            className="flex items-center px-4 py-2 bg-red-600 text-white rounded-md text-sm font-medium shadow-lg border border-red-700 hover:bg-red-700 hover:scale-105 transition-all duration-300 ease-in-out"
                                         >
-                                            <FiTrash2 className="mr-2" size={18} /> Eliminar
+                                            <FiTrash2 className="mr-2 text-white" size={16} /> Eliminar
                                         </button>
                                     </td>
                                 </tr>
@@ -169,7 +185,7 @@ const SearchUsers = () => {
 
             <button
                 onClick={() => navigate("/admin-panel")}
-                className="mt-5 flex items-center px-5 py-3 bg-blue-500 text-white font-bold rounded-full shadow-lg hover:bg-blue-600 transition"
+                className="mt-6 flex items-center px-5 py-3 bg-gradient-to-r from-blue-600 to-blue-600 text-white font-bold rounded-full shadow-lg hover:shadow-xl hover:from-blue-600 hover:to-blue-700 transition-all"
             >
                 <FiHome className="mr-3" /> Regresar al Panel
             </button>
