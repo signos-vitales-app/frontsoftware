9e137a80db2583212aa07830e05eba18579a1532
signos-vitales-app <Signosvitales.la2@gmail.com>
2024-12-27 21:05:13 -0500

    trazabilidad

--- src/pages/TrazabilidadPage.jsx
+++ src/pages/TrazabilidadPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchTrazabilidad } from "../services/trazabilidadService";
-import { FaSearch, FaInfoCircle, FaCheck, FaTimes, FaFileExport, FaListAlt, FaFilter, FaArrowLeft } from "react-icons/fa"
+import { FaSearch, FaInfoCircle, FaCheck, FaTimes, FaFileExport, FaListAlt, FaFilter} from "react-icons/fa";
 import { FiHome } from "react-icons/fi";
 import DetalleTrazabilidadModal from "../components/DetalleTrazabilidadModal";
 import generatePDFTrazabilidad from "../services/generatePDFTrazabilidad";
@@ -45,12 +45,10 @@ const TrazabilidadPage = () => {
       const startDate = fechaInicio ? new Date(`${fechaInicio}T00:00:00`) : null;
       const endDate = fechaFin ? new Date(`${fechaFin}T23:59:59`) : null;
 
-      // Verificar si la fecha del registro está dentro del rango, si las fechas están definidas
-      const isWithinDateRange =
+      return (
         (!startDate || itemDate >= startDate) &&
-        (!endDate || itemDate <= endDate);
-
-      return isWithinDateRange;
+        (!endDate || itemDate <= endDate)
+      );
     });
 
     const filteredByUserAndDates = filteredByDates.filter((item) =>
@@ -94,7 +92,11 @@ const TrazabilidadPage = () => {
 
   const handleDeselectAll = () => {
     setSelectedActions({});
-    setMensajeSeleccion("Se han deseleccionado todas las acciones.");
+    setMensajeSeleccion(
+      <span className="text-red-500 font-bold">
+        Se han deseleccionado todas las acciones.
+      </span>
+    );
     setTimeout(() => setMensajeSeleccion(""), 3000);
   };
 
@@ -130,6 +132,7 @@ const TrazabilidadPage = () => {
     setSearchQuery(""); // Restablecer campo de búsqueda
     setFilteredTrazabilidad(trazabilidad); // Mostrar todos los registros
   };
+
   const actionStyles = {
     Creación: { text: "text-green-600 font-bold", dot: "bg-green-600" },
     "Actualización de datos del paciente": { text: "text-yellow-600 font-bold", dot: "bg-yellow-600" },
@@ -145,192 +148,218 @@ const TrazabilidadPage = () => {
   return (
     <div className="trazabilidad-page bg-gray-100 min-h-screen flex flex-col items-center p-12">
       <div className="w-full max-w-6xl">
+        {/* Título */}
         <div className="mb-6 text-center">
-          <div className="flex flex-col items-center space-y-6">
-            <h1 className="text-4xl font-bold text-blue-600">Trazabilidad de Usuarios</h1>
+          <h1 className="text-4xl font-bold text-blue-600">Trazabilidad de Usuarios</h1>
+        </div>
 
-            <div className="grid grid-cols-3 gap-4 items-center mb-6">
+        <div className="bg-white shadow-lg rounded-lg p-6">
+          {/* Filtros */}
+          <div className="flex flex-wrap items-center justify-center space-x-6 mb-6">
+            {/* Fecha de Inicio */}
+            <div className="flex items-center">
+              <label
+                htmlFor="fechaInicio"
+                className="mr-2 text-gray-700 font-semibold text-sm"
+              >
+                Fecha de inicio:
+              </label>
               <input
+                id="fechaInicio"
                 type="date"
-                className="border border-gray-300 p-3 rounded w-full"
-                placeholder="dd/mm/aaaa"
+                className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 value={fechaInicio}
                 onChange={(e) => setFechaInicio(e.target.value)}
               />
+            </div>
+
+            {/* Fecha de Fin */}
+            <div className="flex items-center">
+              <label
+                htmlFor="fechaFin"
+                className="mx-2 text-gray-700 font-semibold text-sm"
+              >
+                Fecha de fin:
+              </label>
               <input
+                id="fechaFin"
                 type="date"
-                className="border border-gray-300 p-3 rounded w-full"
-                placeholder="dd/mm/aaaa"
+                className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 value={fechaFin}
                 onChange={(e) => setFechaFin(e.target.value)}
               />
-              <div className="relative w-full">
-                <span className="absolute inset-y-0 left-3 flex items-center text-gray-500">
+            </div>
+
+            {/* Buscar Usuario */}
+            <div className="flex items-center">
+              <label
+                htmlFor="buscarUsuario"
+                className="mx-2 text-gray-700 font-semibold text-sm"
+              >
+                Buscar usuario:
+              </label>
+              <div className="relative">
+                <span className="absolute inset-y-0 left-3 flex items-center text-gray-400">
                   <FaSearch />
                 </span>
                 <input
+                  id="buscarUsuario"
                   type="text"
-                  placeholder="Buscar por usuario..."
-                  className="border border-gray-300 p-3 pl-10 rounded w-full"
+                  placeholder="Ingrese el nombre"
+                  className="p-2 pl-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                   value={searchQuery}
                   onChange={(e) => setSearchQuery(e.target.value)}
                 />
               </div>
             </div>
+          </div>
 
-            <div className="flex space-x-4 justify-start">
-              <button
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition flex items-center space-x-2"
-                onClick={handleSearch}
-              >
-                <FaFilter className="text-sm" />
-                <span>Filtrar</span>
-              </button>
-              <button
-                className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition flex items-center space-x-2"
-                onClick={handleClearFilters}
-              >
-                <FaTimes className="text-sm" />
-                <span>Limpiar Filtros</span>
-              </button>
-            </div>
+          <div className="flex justify-center space-x-4 mb-6">
+            <button
+              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 flex items-center space-x-2"
+              onClick={handleSearch}
+            >
+              <FaFilter />
+              <span>Filtrar</span>
+            </button>
+            <button
+              className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 flex items-center space-x-2"
+              onClick={handleClearFilters}
+            >
+              <FaTimes />
+              <span>Limpiar Filtros</span>
+            </button>
+          </div>
 
-            <div className="overflow-x-auto bg-white shadow-lg rounded-lg">
-              {loading && <p className="text-center text-blue-500 py-4">Cargando registros...</p>}
-              {error && <p className="text-center text-red-500 py-4">{error}</p>}
-              {filteredTrazabilidad.length > 0 ? (
-                <table className="table-auto w-full border-collapse bg-white text-center">
-                  <thead>
-                    <tr className="bg-blue-600 text-white">
-                      <th className="p-4">Seleccionar</th>
-                      <th className="p-4">Usuario</th>
-                      <th className="p-4">Acción</th>
-                      <th className="p-4">Fecha - Hora</th>
-                      <th className="p-4">Detalles</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {filteredTrazabilidad.map((item, index) => (
-                      <tr
-                        key={item.id}
-                        className={`${selectedActions[item.usuario_nombre]?.includes(item.id)
+          <div className="overflow-x-auto bg-white shadow rounded-lg">
+            {loading && <p className="text-center text-blue-500 py-4">Cargando registros...</p>}
+            {error && <p className="text-center text-red-500 py-4">{error}</p>}
+            {filteredTrazabilidad.length > 0 ? (
+              <table className="table-auto w-full border-collapse bg-white text-center">
+                <thead>
+                  <tr className="bg-blue-600 text-white">
+                    <th className="p-4">Seleccionar</th>
+                    <th className="p-4">Usuario</th>
+                    <th className="p-4">Acción</th>
+                    <th className="p-4">Fecha - Hora</th>
+                    <th className="p-4">Detalles</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {filteredTrazabilidad.map((item, index) => (
+                    <tr
+                      key={item.id}
+                      className={`$ {
+                        selectedActions[item.usuario_nombre]?.includes(item.id)
                           ? "bg-blue-100"
                           : index % 2 === 0
-                            ? "bg-white"
-                            : "bg-gray-50"
-                          } hover:bg-blue-100`}
+                          ? "bg-white"
+                          : "bg-gray-50"
+                      } hover:bg-blue-100`}
+                    >
+                      <td className="p-5 text-center align-middle">
+                        <input
+                          type="checkbox"
+                          checked={selectedActions[item.usuario_nombre]?.includes(item.id)}
+                          onChange={() => handleSelectAction(item.usuario_nombre, item.id)}
+                        />
+                      </td>
+                      <td className="p-5 py-3 text-center align-middle">{item.usuario_nombre}</td>
+                      <td
+                        className={`p-5 text-center align-middle flex items-center justify-center space-x-2 ${getActionStyle(
+                          item.accion
+                        )}`}
                       >
-                        <td className="p-5 text-center align-middle">
-                          <input
-                            type="checkbox"
-                            checked={selectedActions[item.usuario_nombre]?.includes(item.id)}
-                            onChange={() => handleSelectAction(item.usuario_nombre, item.id)}
-                          />
-                        </td>
-                        <td className="p-5 py-3 text-center align-middle">{item.usuario_nombre}</td>
-                        <td
-                          className={`p-5 text-center align-middle flex items-center justify-center space-x-2 ${getActionStyle(
-                            item.accion
-                          )}`}
+                        <span
+                          className={`inline-block w-3 h-3 rounded-full ${getDotStyle(item.accion)}`}
+                        ></span>
+                        <span>{item.accion}</span>
+                      </td>
+                      <td className="p-5 text-center align-middle">{new Date(item.fecha_hora).toLocaleString()}</td>
+                      <td className="p-5 text-center align-middle">
+                        <button
+                          className="bg-blue-500 text-white px-4 py-2 rounded shadow hover:bg-blue-600 transition"
+                          onClick={() => {
+                            setSelectedTrazabilidadId(item.id);
+                            setIsModalOpen(true);
+                          }}
                         >
-                          <span
-                            className={`inline-block w-3 h-3 rounded-full ${getDotStyle(item.accion)}`}
-                          ></span>
-                          <span>{item.accion}</span>
-                        </td>
-
-                        <td className="p-5 text-center align-middle">{new Date(item.fecha_hora).toLocaleString()}</td>
-                        <td className="p-5 text-center align-middle">
-                          <button
-                            className="bg-blue-500 text-white px-4 py-2 rounded shadow hover:bg-blue-600 transition"
-                            onClick={() => {
-                              setSelectedTrazabilidadId(item.id);
-                              setIsModalOpen(true);
-                            }}
-                          >
-                            <FaInfoCircle className="inline mr-2" /> Ver detalles
-                          </button>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              ) : (
-                !loading &&
-                !error &&
-                trazabilidad.length === 0 && (
-                  <div className="text-center py-12 flex flex-col items-center bg-white p-6 rounded-lg shadow-md">
-                    <p className="text-red-500 text-lg font-semibold flex items-center">
-                      <span role="img" aria-label="alert" className="mr-2">
-                        🚫
-                      </span>
-                      No hay registros de trazabilidad disponibles.
-                    </p>
-                    <p className="text-gray-400 text-sm mt-2">
-                      Verifica el usuario o intenta realizar una nueva búsqueda.
-                    </p>
-                  </div>
-                )
-              )}
-            </div>
-
-            {generatingPDF && (
-              <div className="text-center text-blue-500 mt-4 font-bold">Generando PDF...</div>
+                          <FaInfoCircle className="inline mr-2" /> Ver detalles
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              !loading && (
+                <div className="text-center text-red-500 py-6">
+                  {searchQuery
+                    ? `No hay usuarios registrados con el nombre '${searchQuery}'.`
+                    : fechaInicio || fechaFin
+                      ? "No hay trazabilidad en el rango de fechas seleccionado, verifica nuevamente."
+                      : "No hay registros disponibles."}
+                </div>
+              )
             )}
+          </div>
 
-            <div className="flex justify-center space-x-4 mt-4">
-              <button
-                className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-green-600 transition flex items-center space-x-2"
-                onClick={handleSelectAllFiltered}
-              >
-                <FaListAlt /> {/* Icono de lista */}
-                <span>Seleccionar Todo</span>
-              </button>
-              <button
-                className="bg-red-500 text-white px-6 py-2 rounded hover:bg-red-600 transition flex items-center space-x-2"
-                onClick={handleDeselectAll}
-              >
-                <FaTimes /> {/* Icono de cruz */}
-                <span>Quitar Selecciones</span>
-              </button>
-              <button
-                className="bg-gray-500 text-white px-6 py-2 rounded hover:bg-green-600 transition flex items-center space-x-2"
-                onClick={() => handleExportarPDF(false)}
-              >
-                <FaCheck /> {/* Icono de cheque */}
-                <span>Exportar Seleccionados</span>
-              </button>
-              <button
-                className="bg-gray-500 text-white px-6 py-2 rounded hover:bg-green-600 transition flex items-center space-x-2"
-                onClick={() => handleExportarPDF(true)}
-              >
-                <FaFileExport /> {/* Icono de exportar */}
-                <span>Exportar Todo</span>
-              </button>
+          {mensajeSeleccion && (
+            <div className="text-center text-green-500 font-bold py-4">
+              {mensajeSeleccion}
             </div>
+          )}
 
-            {/* Botón para volver al panel */}
-            <div className="flex justify-center mt-4">
-              <button
-                type="button"
-                onClick={() => navigate('/admin-panel')}
-                className="flex items-center px-6 py-3 bg-blue-600 text-white font-bold rounded-md hover:bg-blue-700 transition duration-300 shadow-md"
-              >
-                <FiHome size={20} className="mr-2" /> Regresar al Panel
-              </button>
-            </div>
-
-            <DetalleTrazabilidadModal
-              isOpen={isModalOpen}
-              onClose={() => setIsModalOpen(false)}
-              trazabilidadId={selectedTrazabilidadId}
-            />
+          <div className="flex justify-center space-x-4 mt-4">
+            <button
+              className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-green-600 transition flex items-center space-x-2"
+              onClick={handleSelectAllFiltered}
+            >
+              <FaListAlt />
+              <span>Seleccionar Todo</span>
+            </button>
+            <button
+              className="bg-red-500 text-white px-6 py-2 rounded hover:bg-red-600 transition flex items-center space-x-2"
+              onClick={handleDeselectAll}
+            >
+              <FaTimes />
+              <span>Quitar Selecciones</span>
+            </button>
+            <button
+              className="bg-green-500 text-white px-6 py-2 rounded hover:bg-green-600 transition flex items-center space-x-2"
+              onClick={() => handleExportarPDF(false)}
+            >
+              <FaCheck />
+              <span>Exportar Seleccionados</span>
+            </button>
+            <button
+              className="bg-gray-500 text-white px-6 py-2 rounded hover:bg-green-600 transition flex items-center space-x-2"
+              onClick={() => handleExportarPDF(true)}
+            >
+              <FaFileExport />
+              <span>Exportar Todo</span>
+            </button>
           </div>
         </div>
+
+        <div className="flex justify-center mt-4">
+          <button
+            type="button"
+            onClick={() => navigate('/admin-panel')}
+            className="flex items-center px-6 py-3 bg-blue-600 text-white font-bold rounded-md hover:bg-blue-700 transition duration-300 shadow-md"
+          >
+            <FiHome size={20} className="mr-2" /> Regresar al Panel
+          </button>
+        </div>
       </div>
+
+      <DetalleTrazabilidadModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        trazabilidadId={selectedTrazabilidadId}
+      />
     </div>
   );
-
 };
 
-export default TrazabilidadPage; 
+export default TrazabilidadPage;
